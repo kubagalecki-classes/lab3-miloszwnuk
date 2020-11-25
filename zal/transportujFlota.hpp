278cd8481f47ea76fed10e2f5d387a98461c7ba7
miloszwnuk <01157070@pw.edu.pl>
2020-11-25 22:55:58 +0000

    a

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,36 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+
+    int N = 0;
+    for (int i = 1; N<=towar; ++i ) 
+    {
+        if (N=0) 
+        {
+            std::cout << "nie ma towaru do transportu" << std::endl;
+        }
+        else {
+        
+            s* numer     = new s[i];
+            t* transport = new t[i];
+
+            Stocznia stocznia{};
+            Statek*  s[i];
+            double   t[i] = s[i]->transportuj();
+
+            N = N + c;
+
+            delete[] numer;
+            delete[] transport;
+            delete s[i];
+            delete t[i];
+        
+        }
+        
+    }
+
+   
+
+
     return 0;
 }
